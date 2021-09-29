1fea6507dd2e24ecb2d575e3e52a4a457cc7a96f
Yvinzo <geras12@hotmail.com>
2021-09-29 17:18:57 -0500

    Ejemplo de ajustes a Profile de PowerShell

---
+++ profile.ps1
@@ -0,0 +1,12 @@
+﻿#profile1.ps1
+#area para alias y variables
+set-alias procesos -value Get-Process
+$usuario = $env:USERNAME
+# area para definición de funciones
+function hora {get-date}
+function prompt {'PS ' + $(get-location) + ' '+$(date) + ' > '}
+#
+# otras areas utiles para el usuario
+# mensaje de bienvenida al ejecutar el shell
+$user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+write-host Hola $($user.name), bienvenido a PowerShell
